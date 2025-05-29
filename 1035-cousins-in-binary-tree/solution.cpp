4897c9fa269d29dbe846c57bd06b301de2ae1853
brithin03-stack <brithin03@gmail.com>
2025-05-29 04:12:56 +0000

    Sync LeetCode submission - Cousins in Binary Tree - Runtime - 3 ms (3.31%), Memory - 14.4 MB (5.15%)

---
+++ 1035-cousins-in-binary-tree/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+ {
+public:
+    map<int, int> depth;
+    map<int,TreeNode*>parent;
+    void dfs(TreeNode* root,TreeNode* par)
+    {
+        if(root!=NULL)
+       { 
+        depth[root->val]=(par!=NULL)? depth[par->val]+1:0;
+        parent[root->val]=par;
+        dfs(root->left,root);
+        dfs(root->right,root);
+       }
+    }
+    bool isCousins(TreeNode* root, int x, int y) 
+     
+     {
+        dfs(root,NULL);
+        if(depth[x]==depth[y]&&parent[x]!=parent[y])
+            return true;
+        return false;
+     }
+    
+    
+};
